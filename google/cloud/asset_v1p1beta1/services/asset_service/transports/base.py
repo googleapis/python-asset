9066ad8ed9ff65163c55209fe1bd5396ed1a4837
yoshi-automation <yoshi-automation@google.com>
2020-08-11 05:20:39 -0700

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- google/cloud/asset_v1p1beta1/services/asset_service/transports/base.py
+++ google/cloud/asset_v1p1beta1/services/asset_service/transports/base.py
@@ -105,7 +105,7 @@ class AssetServiceTransport(abc.ABC):
                     maximum=60.0,
                     multiplier=1.3,
                     predicate=retries.if_exception_type(
-                        exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
+                        exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
                     ),
                 ),
                 default_timeout=15.0,
@@ -118,7 +118,7 @@ class AssetServiceTransport(abc.ABC):
                     maximum=60.0,
                     multiplier=1.3,
                     predicate=retries.if_exception_type(
-                        exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
+                        exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
                     ),
                 ),
                 default_timeout=15.0,
