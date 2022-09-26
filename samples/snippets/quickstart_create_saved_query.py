b126b7e0b6e65799833cd8a7813fe61a3a194268
jeffreyaiatgoogle <jeffreyai@google.com>
2022-09-26 15:34:05 -0700

    feat: Adds code examples to create, get, list, update and delete a SavedQuery.

---
+++ samples/snippets/quickstart_create_saved_query.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+# Copyright 2019 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import argparse
+
+
+def create_saved_query(project_id, saved_query_id, description):
+    # [START asset_quickstart_create_saved_query]
+    from google.cloud import asset_v1
+
+    # TODO project_id = 'Your Google Cloud Project ID'
+    # TODO saved_query_id = 'SavedQuery ID you want to create'
+    scope = "organizations/474566717491"  # Change to your org, folder. or project
+
+    client = asset_v1.AssetServiceClient()
+    parent = "projects/{}".format(project_id)
+    saved_query = asset_v1.SavedQuery()
+    saved_query.description = description
+    saved_query.content.iam_policy_analysis_query.scope = scope
+    # TODO: add more details into your saved query here.
+
+    response = client.create_saved_query(
+        request={"parent": parent, "saved_query_id": saved_query_id, "saved_query": saved_query}
+    )
+    print("saved_query: {}".format(response))
+    # [END asset_quickstart_create_saved_query]
+    return response
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument("project_id", help="Your Google Cloud project ID")
+    parser.add_argument("saved_query_id", help="SavedQuery ID you want to create")
+    parser.add_argument("description", help="The description of the saved_query")
+    args = parser.parse_args()
+    create_saved_query(args.project_id, args.saved_query_id, args.description)
