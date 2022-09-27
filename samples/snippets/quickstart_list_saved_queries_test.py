129b1194e8cb4ce3459ea8663c5f49ca12238739
jeffreyaiatgoogle <113950621+jeffreyaiatgoogle@users.noreply.github.com>
2022-09-27 15:45:21 -0700

    Update samples/snippets/quickstart_list_saved_queries_test.py
    Co-authored-by: Dan Lee <71398022+dandhlee@users.noreply.github.com>

--- samples/snippets/quickstart_list_saved_queries_test.py
+++ samples/snippets/quickstart_list_saved_queries_test.py
@@ -22,7 +22,7 @@ PROJECT = os.environ["GOOGLE_CLOUD_PROJECT"]
 
 
 def test_list_saved_queries(capsys):
-    parent_resource = "projects/{}".format(PROJECT)
+    parent_resource = f"projects/{PROJECT}"
     quickstart_list_saved_queries.list_saved_queries(parent_resource)
     out, _ = capsys.readouterr()
     assert "saved_queries" in out
