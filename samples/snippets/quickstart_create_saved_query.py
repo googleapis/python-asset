47de6918a72aa295a9a9d43f88fd33cb7b440e63
jeffreyaiatgoogle <113950621+jeffreyaiatgoogle@users.noreply.github.com>
2022-09-27 15:44:14 -0700

    Update samples/snippets/quickstart_create_saved_query.py
    Co-authored-by: Dan Lee <71398022+dandhlee@users.noreply.github.com>

--- samples/snippets/quickstart_create_saved_query.py
+++ samples/snippets/quickstart_create_saved_query.py
@@ -34,7 +34,11 @@ def create_saved_query(project_id, saved_query_id, description):
     # TODO: add more details into your saved query here.
 
     response = client.create_saved_query(
-        request={"parent": parent, "saved_query_id": saved_query_id, "saved_query": saved_query}
+        request = {
+            "parent": parent,
+            "saved_query_id": saved_query_id,
+            "saved_query": saved_query,
+        }
     )
     print("saved_query: {}".format(response))
     # [END asset_quickstart_create_saved_query]
